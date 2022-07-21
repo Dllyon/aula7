8f94b95e740fa148c12ea830869842468c75358f
Debora Silva <deborafllyon@gmail.com>
2022-07-20 22:49:29 -0300

    Concluido

---
+++ aula7/EqXn.java
@@ -0,0 +1,37 @@
+package aula7;
+
+public class EqXn extends Equacao {
+  private double xn;
+  private double xp;
+
+  // #region Métodos de Acesso
+  public double getXn() {
+    return xn;
+  }
+
+  public void setXn(double xn) {
+    this.xn = xn;
+  }
+
+  public double getXp() {
+    return xp;
+  }
+
+  public void setXp(double xp) {
+    this.xp = xp;
+  }
+  // #endregion
+
+  // #region Regras de Negócio
+  public double calculaXn(Delta delta) {
+    xn = (-delta.getB() - Math.sqrt(delta.getDelta()));
+    return xn / (2 * delta.getA());
+  }
+
+  public double calculaXp(Delta delta) {
+    xp = (-delta.getB() + Math.sqrt(delta.getDelta()));
+    return xp / (2 * delta.getA());
+  }
+  // #endregion
+
+}
